d102172c6b170f278cfa06b39e4d08eb274f1d06
DanielIbrag <danny.ibragimov1@gmail.com>
2022-04-20 19:54:39 -0400

    big update

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,21 +1,32 @@
 class CartedProductsController < ApplicationController
+  def index
+    carted_products = current_user.carted_products.where(status: "carted")
+    render json: carted_products.as_json
+  end
+
   def create
-    carted_products = CartedProduct.new(
+    carted_product = CartedProduct.new(
       user_id: current_user.id,
-      product_id: params["product_id"],
-      quantity: params["quantity"],
-      status: "carted",
+      product_id: params[:product_id],
       order_id: nil,
+      quantity: params[:quantity],
+      status: "carted",
     )
-    if carted_products.save
-      render json: carted_products.as_json
+
+    if carted_product.save
+      render json: carted_product.as_json
     else
-      render json: { error_messages: carted_products.errors.full_messages }, status: 422
+      render json: { error_message: carted_product.errors.full_messages }, status: 422
     end
   end
 
-  def index
-    carted_products = current_user.carted_products
-    render json = carted_products.as_json
+  def destroy
+    carted_product = CartedProduct.find(params[:id])
+    carted_product.status = "removed"
+    if carted_product.save
+      render json: { status: "Carted Prodcut remove!" }
+    else
+      render json: { status: "oh no, something went wrong" }
+    end
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,20 +7,30 @@ class OrdersController < ApplicationController
   end
 
   def create
-    product = Product.find_by(id: params[:product_id])
-    calculated_subtotal = product.price * params[:quantity].to_i
+    # product = Product.find_by(id: params[:product_id])
+
+    # calculated_subtotal = product.price * params[:quantity].to_i
+    # calculated_tax = calculated_subtotal * 0.09
+    # calculated_total = calculated_subtotal + calculated_tax
+
+    carted_products = current_user.carted_products.where(status: "carted")
+
+    calculated_subtotal = 0
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+    end
     calculated_tax = calculated_subtotal * 0.09
     calculated_total = calculated_subtotal + calculated_tax
 
     order = Order.new(
       user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
       subtotal: calculated_subtotal,
       tax: calculated_tax,
       total: calculated_total,
     )
+
     if order.save
+      carted_products.update_all(status: "purchased", order_id: order.id)
       render json: order.as_json
     else
       render json: { error_messages: order.errors.full_messages }, status: 422
@@ -28,7 +38,7 @@ class OrdersController < ApplicationController
   end
 
   def show
-    single_route = current_user.orders.find_by(id: params[:id])
-    render json: single_route.as_json
+    order = current_user.orders.find_by(id: params[:id])
+    render json: order.as_json
   end
 end
